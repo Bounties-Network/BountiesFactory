c7a3409c201d79805f1623b009c919977ae15557
mbeylin <mark_beylin@hotmail.com>
2018-05-17 16:47:49 -0400

    fixed accounts

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -193,7 +193,7 @@ class UserPage extends Component {
               this.setState({modalError: "Please unlock your MetaMask Accounts", modalOpen: true});
 
             } else {
-            var account = web3.eth.accounts[0];
+            var account = accs[0];
             setInterval(function() {
               web3.eth.getAccounts(function(err, accs){
                 if (accs[0] !== account) {
